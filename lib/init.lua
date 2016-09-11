587ffcb05559d05deec771e4d701286c3519a0e0
Lord Niels N. Horn <i.am@nilq.dk>
2016-09-11 19:15:02 +0200

    Turn valyrian into a gamedev framework

---
+++ lib/init.lua
@@ -0,0 +1,62 @@
+require "lib/valyrian"
+
+love.graphics.setDefaultFilter("nearest", "nearest")
+
+function love.run()
+  local dt = 0
+
+  local update_timer = 0
+  local target_delta = 1 / 60
+
+  if love.math then
+    love.math.setRandomSeed(os.time())
+  end
+
+  if love.load then
+    emit:load()
+  end
+
+  if love.timer then
+    love.timer.step()
+  end
+
+  while true do
+    update_timer = update_timer + dt
+
+    if love.event then
+      love.event.pump()
+      for name, a, b, c, d, e, f in love.event.poll() do
+        event:name(a, b, c, d, e, f)
+        if name == "quit" then
+          if not love.quit or not love.quit() then
+            return a
+          end
+        end
+      end
+    end
+
+    if love.timer then
+      love.timer.step()
+      dt = love.timer.getDelta()
+    end
+
+    if update_timer > target_delta then
+      event:update(target_delta)
+    end
+
+    if love.graphics and love.graphics.isActive() then
+      love.graphics.clear(love.graphics.getBackgroundColor())
+      love.graphics.setColor(255, 255, 255)
+
+      love.graphics.origin()
+
+      event:draw()
+
+      love.graphics.present()
+    end
+
+    if love.timer then
+      love.timer.sleep(0.001)
+    end
+  end
+end
---
+++ lib/valyrian.lua
@@ -0,0 +1,150 @@
+require "lib/deepcopy"
+
+local _things = {}
+local _instances = {}
+
+local function call_index(t, func, ...)
+  local args = {...}
+  if #args > 0 then
+    return getmetatable(t).__index(nil, func)(nil, ...)
+  end
+  return function(...)
+    return getmetatable(t).__index(nil, func)(nil, ...)
+  end
+end
+
+local _type = type
+function type(n)
+  local mt = {}
+  if _type(n) == "table" then
+    mt = getmetatable(n) or mt
+  end
+  if _type(rawget(mt, "__type")) == "string" then
+    return rawget(meta, "__type")
+  end
+  return _type(n)
+end
+
+class = setmetatable({}, {
+  __call     = call_index,
+  __newindex = function() end,
+  __type     = "class",
+  __index    = function(_, k)
+    return function(_, ...)
+      if _things[k] then
+        error("trying to redefine class '" .. k .. "'!")
+      end
+
+      local parents = {...}
+
+      return function(body)
+        if type(body) == "table" then
+          if body.self ~= nil then
+            error "class can't contain 'self'!"
+          end
+          _things[k] = body
+
+          setmetatable(_things[k], {
+            __parents = parents,
+          })
+
+          for _, p in ipairs(parents) do
+            for n, v in pairs(_things[p]) do
+              if _things[k][n] == nil then
+                _things[k][n] = v
+              end
+            end
+          end
+        else
+          error("trying to define class '" .. k .. "' with no body!")
+        end
+      end
+    end
+  end,
+})
+
+new = setmetatable({}, {
+  __call     = call_index,
+  __newindex = function() end,
+  __index    = function(_, n)
+    return function(_, ...)
+      if not _things[n] then
+        error("trying to instantiate undefined class '" .. n .. "'!")
+      end
+      local instance = {}
+      local data     = table.deepcopy(_things[n], { function_env = instance }, {
+        ["userdata"] = function(stack, orig, copy, state, arg1, arg2)
+          return orig, true
+        end,
+      })
+      _instances[instance] = setmetatable(instance, {
+        __index = function(t, k)
+          if k == "self" then
+            return instance
+          else
+            return data[k] or (_ENV or _G)[k]
+          end
+        end,
+        __newindex = function(t, k, v)
+          data[k] = v
+        end,
+        __type = n,
+        __parents = getmetatable(_things[n]).__parents,
+      })
+      if instance.awake then
+        instance.awake(...)
+      end
+      return instance
+    end
+  end,
+})
+
+kill = function(instance, ...)
+  if not instance then
+    error "trying to remove nil!"
+  end
+  _instances[instance] = nil
+  if instance.kill then
+    return instance.kill(...)
+  end
+end
+
+event = setmetatable({}, {
+  __call     = call_index,
+  __newindex = function() end,
+  __index    = function(_, method)
+    return function(_, ...)
+      local r = {}
+      for instance in pairs(_instances) do
+        if instance[method] then
+          r[instance] = instance[method](...)
+        end
+      end
+      return r
+    end
+  end,
+})
+
+super = setmetatable({}, {
+  __call     = call_index,
+  __newindex = function() end,
+  __index    = function(_, value)
+    return function(_, n, p, ...)
+      local r = {}
+      local mt = getmetatable(_instances[n])
+
+      for k, v in pairs(mt.__parents) do
+        if v == p then
+          local v_type = type(_things[v][value])
+          if v_type == "function" then
+            return _things[v][value](...)
+          end
+          return _things[v][value]
+        else
+          error(p .. " is not a parent of " .. mt.__type .. "!")
+        end
+      end
+      return r
+    end
+  end
+})
