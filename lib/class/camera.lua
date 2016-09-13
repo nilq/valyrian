a2797c6a48d8d6131f6fe76457801f6fbd195ef0
nilq <i.am@nilq.dk>
2016-09-13 11:14:21 +0200

    Add camera class

--- lib/class/camera.lua
+++ lib/class/camera.lua
@@ -1,3 +1,24 @@
-class: Camera() {
+class: Camera("Transform2D") {
+  set = function()
+    love.graphics.push()
+    love.graphics.rotate(-self.rotation)
+    love.graphics.scale(1 / self.scale.x, 1 / self.scale.y)
+    love.graphics.translate(-self.position.x, -self.position.y)
+  end;
 
+  unset = function()
+    love.graphics.pop()
+  end;
+
+  view_width = function()
+    love.graphics.getWidth() * self.scale.x
+  end;
+
+  view_height = function()
+    love.graphics.getHeight() * self.scale.y
+  end;
+
+  view_size = function()
+    return self.view_width(), self.view_height()
+  end
 }
